c746264514f8985eafd2464afca9f5862f4839b7
“Reza” <reza_sahjpaul@hotmail.com>
2023-08-29 11:56:53 -0700

    commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(x, z) {
+  return x+z
 	
 };
 
-const subtract = function() {
+const subtract = function(x, z) {
+
+  return x-z
 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(item => {
+      sum += item;
+  });
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = 1;
+  arr.forEach(item => {
+      result *= item;
+  });
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+
+	    return Math.pow(x, y);
+
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+}
+return n * factorialRecursive(n - 1);
 	
 };
 
