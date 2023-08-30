031041da0aa0db9cd52411e2ffa7b363e873229d
“Reza” <reza_sahjpaul@hotmail.com>
2023-08-29 19:56:26 -0700

    commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    
+  const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  return cleanedWord === cleanedWord.split("").reverse().join("");
 };
 
 // Do not edit below this line
